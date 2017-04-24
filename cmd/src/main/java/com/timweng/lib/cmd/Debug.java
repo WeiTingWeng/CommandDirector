a407c2f717d451d51b24ce5b2fb6aa26f7119573
Tim Weng <weng.tim@nuwarobotics.com>
2017-04-24 22:36:46 +0800

    Remove unnecessary functions on Debug

--- cmd/src/main/java/com/timweng/lib/cmd/Debug.java
+++ cmd/src/main/java/com/timweng/lib/cmd/Debug.java
@@ -1,96 +1,45 @@
 
 package com.timweng.lib.cmd;
 
-import android.app.Activity;
 import android.util.Log;
-import android.widget.Toast;
 
 public class Debug {
-    private static String sTag = "CommandDirector";
+    private final static String TAG = "CommandDirector";
     public static boolean sIsShowLog = true;
-    public static boolean sIsShowToast = true;
-
-    private boolean mIsInit = false;
-    private Activity mActivity = null;
-    private Toast mToast;
-
-    private static Debug ourInstance = new Debug();
-
-    public static Debug getInstance() {
-        return ourInstance;
-    }
-
-    private Debug() {
-    }
-
-    public void init(Activity activity, String tag) {
-        mActivity = activity;
-        mToast = Toast.makeText(mActivity, "", Toast.LENGTH_SHORT);
-        sTag = tag;
-        mIsInit = true;
-    }
-
-    public void release() {
-        mIsInit = false;
-        mActivity = null;
-        mToast = null;
-    }
-
-    public void setIsShowLog(boolean isShowLog) {
-        sIsShowLog = isShowLog;
-    }
-
-    public void setIsShowToast(boolean isShowToast) {
-        sIsShowToast = isShowToast;
-    }
-
-    public void showToast(final String string) {
-        if (mIsInit && sIsShowToast && mToast != null) {
-            mActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (mToast != null) {
-                        mToast.setText(string);
-                        mToast.show();
-                    }
-                }
-            });
-        }
-    }
 
     public static void logD(String tag, String msg) {
         if (sIsShowLog) {
-            Log.d(sTag, tag + "." + msg);
+            Log.d(TAG, tag + "." + msg);
         }
     }
 
     public static void logE(String tag, String msg) {
         if (sIsShowLog) {
-            Log.e(sTag, tag + "." + msg);
+            Log.e(TAG, tag + "." + msg);
         }
     }
 
     public static void logE(String tag, String msg, Throwable tr) {
         if (sIsShowLog) {
-            Log.e(sTag, tag + "." + msg, tr);
+            Log.e(TAG, tag + "." + msg, tr);
         }
     }
 
     public static void logI(String tag, String msg) {
         if (sIsShowLog) {
-            Log.i(sTag, tag + "." + msg);
+            Log.i(TAG, tag + "." + msg);
         }
     }
 
     public static void logW(String tag, String msg) {
         if (sIsShowLog) {
-            Log.w(sTag, tag + "." + msg);
+            Log.w(TAG, tag + "." + msg);
         }
     }
 
     public static void logW(String tag, String msg, Throwable tr) {
         if (sIsShowLog) {
-            Log.w(sTag, tag + "." + msg, tr);
+            Log.w(TAG, tag + "." + msg, tr);
         }
     }
 }
